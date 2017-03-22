e693ef4325bf34dce7bdd1a96de68a39ac96ad1e
apwelsh <armand@welshhome.org>
2017-03-21 23:21:19 -0700

    Add missing import for build
    In my Xcode, the .h file does not work without the import of UIKit.

--- UIImage+Resize.h
+++ UIImage+Resize.h
@@ -2,6 +2,7 @@
 // Created by Trevor Harmon on 8/5/09.
 // Free for personal or commercial use, with or without modification.
 // No warranty is expressed or implied.
+@import UIKit;
 
 // Extends the UIImage class to support resizing/cropping
 @interface UIImage (Resize)
